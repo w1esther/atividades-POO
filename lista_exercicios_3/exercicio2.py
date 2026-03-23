58074fdca1b30d1c0c2f4b8c12c433e7e8cb4780
w1esther <w1mariaesther@gmail.com>
2026-03-23 08:03:28 -0300

    conclusao exercicio lista 3

--- lista_exercicios_3/exercicio2.py
+++ lista_exercicios_3/exercicio2.py
@@ -1,11 +1,43 @@
 class ContaBancaria:
 
-    def __init__(self, nome, saldo):
+    def __init__(self, nome):
         
         self.nome_titular = nome 
-        self.__saldo = saldo
+        self.__saldo = 0
 
     @property
     def saldo(self):
 
         return self.__saldo
+
+    def depositar(self, valor):
+
+        if valor <= 0:
+
+            return False
+        
+        else: 
+
+            self.__saldo += valor
+            return self.__saldo
+    
+    def sacar(self, valor):
+
+        if valor > self.__saldo:
+
+            return False
+        
+        else:
+
+            self.__saldo -= valor
+            return self.__saldo
+        
+    def consultar_saldo(self):
+
+        return self.__saldo
+    
+conta1 = ContaBancaria('Maria')
+conta1.depositar(500)
+conta1.sacar(300)
+conta1.depositar(-200)
+print('Saldo atual:', conta1.consultar_saldo())
