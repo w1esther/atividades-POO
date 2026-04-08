f039c1abdeb10666be7e3af47dffaef20c852b65
w1esther <w1mariaesther@gmail.com>
2026-04-08 13:35:56 -0300

    Conclusão lista 04

--- lista_exercicios_4/ex10.py
+++ lista_exercicios_4/ex10.py
@@ -3,6 +3,7 @@ class Aluno:
     def __init__(self, nome):
         self.nome_aluno = nome
 
+
 class Turma:
 
     def __init__(self, nome):
@@ -13,8 +14,11 @@ class Turma:
         self.turma_alunos.append(aluno)
 
     def exibir_alunos(self):
-        for aluno in self.turma_alunos:
-            print(aluno.nome_aluno)
+        if not self.turma_alunos:
+            print(" - Nenhum aluno")
+        else:
+            for aluno in self.turma_alunos:
+                print(f" - {aluno.nome_aluno}")
 
 class Escola:
 
@@ -22,32 +26,31 @@ class Escola:
         self.nome_escola = nome
         self.lista_turmas = []
 
-    def adicionar_turma(self, turma):
+    def criar_turma(self, nome):
+        turma = Turma(nome)
         self.lista_turmas.append(turma)
+        return turma
 
     def listar_turmas(self):
+        print(f"Escola: {self.nome_escola}")
         for turma in self.lista_turmas:
-            print(f'Turma: {turma.nome_turma}')
-            for aluno in turma.turma_alunos:
-                print(f" - {aluno.nome_aluno}")
+            print(f"Turma: {turma.nome_turma}")
+            turma.exibir_alunos()
 
 aluno1 = Aluno("Maria")
 aluno2 = Aluno("João")
 aluno3 = Aluno("Ana")
 aluno4 = Aluno("Carlos")
 
-turma1 = Turma("1º Ano A")
-turma2 = Turma("2º Ano B")
+escola = Escola("IFRN")
+
+turma1 = escola.criar_turma("1º Ano A")
+turma2 = escola.criar_turma("2º Ano B")
 
 turma1.adicionar_aluno(aluno1)
 turma1.adicionar_aluno(aluno2)
 
 turma2.adicionar_aluno(aluno3)
 turma2.adicionar_aluno(aluno4)
-
-escola = Escola("Escola Estadual")
-
-escola.adicionar_turma(turma1)
-escola.adicionar_turma(turma2)
 
 escola.listar_turmas()
